73b5fa857335014dd031b18e48ecca24987e4229
Samuel <samferreira2009@gmail.com>
2026-08-26 14:55:01 -0300

    servidor funcionando e dando resposta pelo ThunderClient

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { login } = require('../controllers/authController');
+router.post('/login', login);
+//quando alguém fizer uma requisição POST para o endereço /login, execute a função
+//login&quot;.
+module.exports = router;
